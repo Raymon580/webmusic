98d8a5866eb3c3c4c1f2a7d79137fe464b5342c6
michaels1968 <87141242+Raymon580@users.noreply.github.com>
2022-08-12 22:32:48 +0100

    nc

--- audify/views.py
+++ audify/views.py
@@ -1,9 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
+@login_required(login_url='/login')
 def index(request):
     return render(request, "audify/index.html")
 
-def login(request):
-    return render(request, "audify/login.html")
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "audify/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
