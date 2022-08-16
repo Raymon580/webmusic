1c5a680f4d51e40dfc63f8525ec33a57c8c9aaaf
michaels1968 <87141242+Raymon580@users.noreply.github.com>
2022-08-16 22:34:31 +0100

    fixed login and register

--- audify/views.py
+++ audify/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
+
+from .models import User
 
 # Create your views here.
 
 @login_required(login_url='/login')
 def index(request):
     return render(request, "audify/index.html")
+
 
 def login_view(request):
     if request.method == "POST":
@@ -23,12 +27,45 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "auctions/login.html", {
+            return render(request, "audify/login.html", {
                 "message": "Invalid username and/or password."
             })
     else:
         return render(request, "audify/login.html")
 
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("login"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmPassword"]
+        if password != confirmation:
+            return render(request, "audify/register.html", {
+                "message": "Passwords must match."
+            })
+        # Ensure password fields are not empty
+        elif password == "" or confirmation == "":
+            return render(request, "audify/register.html", {
+                "message": "Fill out password fields."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "audify/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "audify/register.html")
